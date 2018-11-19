d0d35d2148d0d73d78d28b418a0987976c884e3d
pyxyzn <pyzn@live.cn>
2018-11-19 15:47:12 +0800

    Add db config and adminlte

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=advanced',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 24 * 3600,
+            'schemaCache' => 'cache',
+        ],
     ],
+    'language' => 'zh-CN',
+    'timeZone' => 'Asia/Chongqing',
 ];
